7dbb644546c63f66e00347c232083a5257d46220
ChristopherKlix <70777673+ChristopherKlix@users.noreply.github.com>
2020-10-06 17:39:52 +0200

    update help_custom.py

--- help_custom.py
+++ help_custom.py
@@ -1,5 +1,5 @@
 def help_custom():
-    print('\n  [run] lets you try different logic gates')
+    print('\n  [try] lets you try different logic gates')
     print('\n  [compare] shows truth tables for all logic gates')
     print('\n  [add] lets you add two numbers by using logic gates')
     print('\n  [subtract] lets you subtract two numbers by using logic gates')
