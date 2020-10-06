99eacdbca347860fa4a110163ff58d5d341e3a80
ChristopherKlix <70777673+ChristopherKlix@users.noreply.github.com>
2020-10-06 17:40:14 +0200

    update imports.py

--- imports.py
+++ imports.py
@@ -1,7 +1,11 @@
-from logic_gates import and_gate, or_gate, nand_gate, nor_gate, xor_gate
+# Python dependencies
+from sys import argv
+import re
+
+# local dependencies
 from logic_gate_calculator import add_numbers, subtract_numbers
+from try_custom import try_custom
 from compare import compare
 from explain import explain
 from help_custom import help_custom
-from set_value import set_value
 from color_print import color_print
